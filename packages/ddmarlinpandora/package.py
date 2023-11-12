93e6c87500918a7ddaa3af92630d80d0afadad9c
Federico Meloni <federico.meloni@cern.ch>
2023-11-12 17:10:27 +0100

    adding thomas' ecal corrections

--- packages/ddmarlinpandora/package.py
+++ packages/ddmarlinpandora/package.py
@@ -9,16 +9,19 @@ from spack.pkg.mucoll.mucoll_stack import MCIlcsoftpackage
 class Ddmarlinpandora(CMakePackage, MCIlcsoftpackage):
     """Interface between Marlin and PandoraPFA."""
 
-    homepage = "https://github.com/MuonColliderSoft/DDMarlinPandora"
-    git      = "https://github.com/MuonColliderSoft/DDMarlinPandora.git"
-    url      = "https://github.com/MuonColliderSoft/DDMarlinPandora/archive/refs/tags/v00-14-MC.tar.gz"
+    homepage = "https://github.com/tmadlener/DDMarlinPandora/"
+    git = "https://github.com/tmadlener/DDMarlinPandora.git"
+    url = "https://github.com/tmadlener/DDMarlinPandora/archive/refs/tags/v00-14-MC.tar.gz"
 
-    maintainers = ['gianelle', 'pandreetto']
+    maintainers = ['tmadlener']
 
-    version('master', branch='master')
-    version('0.14',  sha256='b22f1fb589d59b9878d746632ad74ca76a03153b16c8901023eecc7469f6dbdb')
-    version('0.13',  sha256='1e14a89cd408f597f27b2743af7684068457d6f0b0e1737f03e8093d9deb23d3')
-    version('0.12',  sha256='39b0d22b9b4527bce11f3a741f102660d7986dadb4eca085de91f0d256855ddc')
+    version('master', branch='ecal-energy-corrections')
+    version(
+        '0.14',  sha256='b22f1fb589d59b9878d746632ad74ca76a03153b16c8901023eecc7469f6dbdb')
+    version(
+        '0.13',  sha256='1e14a89cd408f597f27b2743af7684068457d6f0b0e1737f03e8093d9deb23d3')
+    version(
+        '0.12',  sha256='39b0d22b9b4527bce11f3a741f102660d7986dadb4eca085de91f0d256855ddc')
 
     depends_on('ilcutil')
     depends_on('marlinutil')
@@ -31,7 +34,8 @@ class Ddmarlinpandora(CMakePackage, MCIlcsoftpackage):
     depends_on('marlintrk')
 
     def setup_run_environment(self, spack_env):
-        spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libDDMarlinPandora.so")
+        spack_env.prepend_path(
+            'MARLIN_DLL', self.prefix.lib + "/libDDMarlinPandora.so")
 
     def cmake_args(self):
         # C++ Standard
