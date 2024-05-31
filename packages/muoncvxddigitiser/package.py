fce4a0d04f7fac2a046f650c5b099e3b5ef59257
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2024-05-31 16:28:21 +0200

    New version for MuonCVXDdigitiser

--- packages/muoncvxddigitiser/package.py
+++ packages/muoncvxddigitiser/package.py
@@ -14,7 +14,8 @@ class Muoncvxddigitiser(CMakePackage, Ilcsoftpackage):
     url      = "https://github.com/MuonColliderSoft/MuonCVXDDigitiser/archive/refs/tags/v00-01.tar.gz"
 
     version("master", branch="master")
-    version("0.1",    sha256="b4fe817025aeda01e0d503a91a5988b4c1d906dfcb02d2a505f013f8de90efc0")
+    version("0.2.0.1", sha256="c33f60ad75c55a530f048714bdca49e9407bd82856161d2fb945b4c2c61b3d1d")
+    version("0.1.0",   sha256="b4fe817025aeda01e0d503a91a5988b4c1d906dfcb02d2a505f013f8de90efc0")
     
 
     depends_on('ilcutil')
