ebc0a21310bea422da1132c997178a3aba1844f8
Federico Meloni <madb4ron@gmail.com>
2025-10-02 09:00:22 +0200

    added back k4actstracking

---
+++ packages/k4actstracking/package.py
@@ -0,0 +1,28 @@
+from spack.package import *
+from spack.pkg.k4.key4hep_stack import Key4hepPackage
+
+
+class K4actstracking(CMakePackage, Key4hepPackage):
+    """Acts tracking components for the key4hep project"""
+
+    homepage = "https://github.com/key4hep/k4ActsTracking"
+    # todo
+    # url = "https://github.com/key4hep/k4ActsTracking"
+    git = "https://github.com/key4hep/k4ActsTracking.git"
+
+    maintainers("vvolkl")
+
+    version("main", branch="main")
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+
+    depends_on("acts")
+    depends_on("gaudi")
+    depends_on("root")
+    depends_on("edm4hep")
+    depends_on("k4fwcore")
+    depends_on("opendatadetector", type="test")
+
+    def cmake_args(self):
+        return []
