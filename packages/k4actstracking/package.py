5e203169c8db4bdd3907a568680fc8a02f69cabf
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2025-10-10 11:50:02 +0200

    New definitions for ACTS processors

---
+++ packages/k4actstracking/package.py
@@ -0,0 +1,30 @@
+from spack.package import *
+from spack.pkg.k4.key4hep_stack import Key4hepPackage
+
+
+class K4actstracking(CMakePackage, Key4hepPackage):
+    """Acts tracking components for the key4hep project"""
+
+    homepage = "https://github.com/key4hep/k4ActsTracking"
+    # todo
+    # url = "https://github.com/key4hep/k4ActsTracking"
+    git = "https://github.com/key4hep/k4ActsTracking.git"
+
+    maintainers("vvolkl")
+
+    version("main", branch="main")
+    version("evo", commit="eaffaff7ddb57c673769371ea7fa7b8ef9eeee1e")
+
+    depends_on("c", type="build")
+    depends_on("cxx", type="build")
+
+    depends_on("acts+dd4hep+tgeo+json")
+    depends_on("gaudi")
+    depends_on("root")
+    depends_on("edm4hep")
+    depends_on("k4fwcore")
+    depends_on("opendatadetector", type="test")
+    depends_on('lua')
+
+    def cmake_args(self):
+        return []
--- packages/marlinacts/package.py
+++ packages/marlinacts/package.py
@@ -1,7 +1,7 @@
 from spack.pkg.mucoll.mucoll_stack import MCIlcsoftpackage
 from spack.package import *
 
-class MarlinACTS(CMakePackage, MCIlcsoftpackage):
+class Marlinacts(CMakePackage, MCIlcsoftpackage):
     """Marlin package for track reconstructions using the ACTS library"""
 
     homepage = "https://github.com/MuonColliderSoft/MarlinACTS"
@@ -14,13 +14,13 @@ class MarlinACTS(CMakePackage, MCIlcsoftpackage):
     version("1.0.3", sha256='52f86c08847537a76eab3f90ca9fe3ceda5a048734743f9ce36c75cdeaf3be1b', preferred=True)
 
     # Ensuring correct ACTS version due to its evolving API
-    depends_on('acts@42.0.0 +dd4hep+tgeo+identification+json+fatras')
-
+    depends_on('acts')
     depends_on('dd4hep')
     depends_on('ilcutil')
     depends_on('marlin')
     depends_on('root')
     depends_on('tbb')
+    depends_on('lua')
 
     # Building in parallel may fail
     parallel = False
@@ -28,9 +28,9 @@ class MarlinACTS(CMakePackage, MCIlcsoftpackage):
     
     def setup_run_environment(self, spack_env):
         spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libMarlinACTS.so")
-        spack_env.set('MarlinACTS_DATA', self.prefix.share.MarlinACTS.data)
-        spack_env.set("ACTS_TGeoFile", self.prefix.share.MarlinACTS.data + "/MuSIC_v2.root")
-        spack_env.set("ACTS_MatFile", self.prefix.share.MarlinACTS.data + "/material-maps.json")
+        spack_env.set('MarlinACTS_DATA', self.prefix.share.Marlinacts.data)
+        spack_env.set("ACTS_TGeoFile", self.prefix.share.Marlinacts.data + "/MuSIC_v2.root")
+        spack_env.set("ACTS_MatFile", self.prefix.share.Marlinacts.data + "/material-maps.json")
 
     def cmake_args(self):
         # C++ Standard
