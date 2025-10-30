e0e8e1443265a59c8af0ddfc660150b292ddce78
Federico Meloni <madb4ron@gmail.com>
2025-10-30 20:47:47 +0100

    add libcurl?

--- packages/mucoll-stack/package.py
+++ packages/mucoll-stack/package.py
@@ -102,6 +102,7 @@ class MucollStack(BundlePackage, Key4hepPackage):
     ############ generic packages ############
     #######################################################
     depends_on('delphes')
+    depends_on('libcurl')
 
     ##################### developer tools #################
     #######################################################
