95dc61dc3a9d3cf3e532bcdc2bc2f6a8ebcb9468
Federico Meloni <madb4ron@gmail.com>
2025-11-05 13:32:53 +0100

    torch scatter

--- packages/mucoll-stack/package.py
+++ packages/mucoll-stack/package.py
@@ -120,8 +120,9 @@ class MucollStack(BundlePackage, Key4hepPackage):
         depends_on('py-onnxruntime')
         depends_on('py-onnx')
         depends_on("py-torch")
-        #depends_on('acorn')
-        #depends_on('torch-scatter')
+        depends_on('acorn')
+        depends_on('torch-scatter')
+        depends_on('py-torch-scatter')
 
     with when('+pytools'):
         # Python tools
