49cc696e8e283ca329e52fbb9d04d0be9ed773e7
Federico Meloni <madb4ron@gmail.com>
2025-11-05 15:32:56 +0100

    add torch_scatter deps

--- packages/torch-scatter/package.py
+++ packages/torch-scatter/package.py
@@ -1,19 +1,21 @@
 from spack_repo.builtin.build_systems.cmake import CMakePackage
+
 from spack.package import *
 
-class TorchScatter(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
 
-    # FIXME: Add a proper url for your package's homepage here.
+class TorchScatter(CMakePackage):
+    """Torch Extension Library of Optimized Scatter Operations.
+
+    This version of the package is consumable by downstream users via CMake
+    """
+
+    homepage = "https://github.com/rusty1s/pytorch_scatter"
     git = "https://github.com/rusty1s/pytorch_scatter"
     url = "https://github.com/rusty1s/pytorch_scatter/archive/refs/tags/2.1.2.tar.gz"
 
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
-
     license("MIT")
 
+    version("master", branch="master")
     version("2.1.2", sha256="6f375dbc9cfe03f330aa29ea553e9c7432e9b040d039b041f08bf05df1a8bf37")
     version("2.1.1", sha256="ea4f4ac4210a983d878df29e82190f60a7e6e9a791adbe0c0d635040e6256c4d")
     version("2.1.0", sha256="c587ddf7ffaf8e4ddc31d8016d7f445d5896ce491209574800166a9f85c29377")
@@ -25,17 +27,20 @@ class TorchScatter(CMakePackage):
     version("2.0.4", sha256="4fdadd6587f16ef3ff63c52f313f0c9dd97d13ae6496867fe566c309a4ea4036")
     version("2.0.3", sha256="ff2ca1468cb4e49b65bea8f889971f196f209231fbee0cc8bd1615ecb367400b")
 
-    #depends_on("cxx", type="build")
-    depends_on("py-torch")
+    variant("cuda", default=False, description="Build with CUDA support")
 
-    # FIXME: Add dependencies if required.
-    # depends_on("foo")
+    depends_on("cxx", type="build")
+    depends_on("c", type="build")
+
+    depends_on("py-torch")
+    depends_on("py-torch +cuda", when="+cuda")
+
+    conflicts("py-torch@2.1:", when="@:2.1.2")
 
     def cmake_args(self):
-        args = []
-        args.append("-DWITH_CUDA=OFF")
-        args.append("-DWITH_PYTHON=ON")
-        args.append("-DCMAKE_BUILD_TYPE=Release")
-        args.append(self.define('CMAKE_CXX_STANDARD', '20'))
-
+        args = [
+            self.define_from_variant("WITH_CUDA", "cuda"),
+            self.define("CMAKE_CXX_STANDARD", "20"),
+            self.define("WITH_PYTHON", True),
+        ]
         return args
