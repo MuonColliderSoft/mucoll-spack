896983600a997451271f480dc9f97bae47e2b645
Federico Meloni <madb4ron@gmail.com>
2025-12-08 11:54:51 +0100

    added trackml

---
+++ packages/py-trackml/package.py
@@ -0,0 +1,14 @@
+from spack_repo.builtin.build_systems.python import PythonPackage
+from spack.package import *
+
+
+class Trackml(PythonPackage):
+    """TrackML utilities for loading, storing, and manipulating TrackML data."""
+
+    homepage = "https://github.com/LAL/trackml-library"
+    git = "https://github.com/LAL/trackml-library.git"
+
+    version('v2', branch='v2')
+
+    depends_on("py-numpy")
+    depends_on("py-pandas")
