71f1f84d5bbbf6a360e835ce561b169236815d7f
Federico Meloni <madb4ron@gmail.com>
2026-06-16 18:12:40 +0200

    Revert "Marlin free?"
    This reverts commit 686861ec2ee9a336e79fa8f38eee9878aba6e37c.

---
+++ packages/mybibutils/package.py
@@ -0,0 +1,32 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.pkg.k4.key4hep_stack import Key4hepPackage
+from spack.package import *
+
+class Mybibutils(CMakePackage, Key4hepPackage):
+    """A collection of legacy BIB-related Processors used in some MAIA studies"""
+
+    homepage = "https://github.com/madbaron/MyBIBUtils"
+    git      = "https://github.com/madbaron/MyBIBUtils.git"
+    url      = "https://github.com/madbaron/MyBIBUtils/archive/refs/tags/v0.1.tar.gz"
+
+    maintainers = ['madbaron']
+
+    version('main', branch='main')
+    version("0.1", sha256="fa21f3dadbcb610e6c372f457e469488d5995c7c74744be3ccc544990b787caf", preferred=True)    
+
+    depends_on('marlin')
+    depends_on('marlinutil')
+    depends_on('root')
+
+    def setup_run_environment(self, spack_env):
+        spack_env.prepend_path('MARLIN_DLL', self.prefix.lib + "/libMyBIBUtils.so")
+
+    def cmake_args(self):
+        return [
+            self.define('CMAKE_CXX_STANDARD',
+                        self.spec['root'].variants['cxxstd'].value)
+        ]
