065459d934c61fcae1ef4a5d1d56b03bd5d3a632
Federico Meloni <madb4ron@gmail.com>
2026-08-30 13:44:20 +0200

    fix typo

--- packages/mucoll-stack/package.py
+++ packages/mucoll-stack/package.py
@@ -77,7 +77,7 @@ class MucollStack(BundlePackage, Key4hepPackage):
         # workflow loads at runtime (with EnableGeant4Geo=False).
         depends_on('k4simgeant4')
         depends_on('k4clue')
-        depends_on('k4recalorimeter')
+        depends_on('k4reccalorimeter')
         depends_on('k4rectracker')
         depends_on('muoncvxddigitiser')
 
